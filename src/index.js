a2972d73e7d8ec49a10e774c91995bf7017f5c2a
stan-radu-andrei <stan.radu.andrei@gmail.com>
2018-10-31 19:47:01 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function component() {
+    let element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
